2d8edefd467d342483270c8a4d3ffdc6680fa6e6
Mary Remo <maryjewel99@gmail.com>
2019-04-02 15:48:48 -0700

    finished employee controller

--- BangazonSprint/Models/Department.cs
+++ BangazonSprint/Models/Department.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BangazonSprintStartUp.Models
+namespace BangazonSprint.Models
 {
     public class Department
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- BangazonSprint/Models/Employee.cs
+++ BangazonSprint/Models/Employee.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using BangazonSprint.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BangazonSprintStartUp.Models
+namespace BangazonSprint.Models
 {
     public class Employee
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public int DepartmentId { get; set; }
         public bool IsSupervisor { get; set; }
+        public Department department { get; set; }
     }
 }
