9f7eb7fce5c96dab9e5a30403f367b245bfb5ca6
hanna <hannahmneal@gmail.com>
2019-04-03 09:28:33 -0500

    minor updates to producttype.cs

--- BangazonSprint/Models/ProductType.cs
+++ BangazonSprint/Models/ProductType.cs
@@ -6,17 +6,10 @@ using System.Threading.Tasks;
 
 namespace BangazonSprint.Models
 {
-    //-------------------------------------------------------------------------------
-    //HMN: Added props to test the functionality of ProductController
-
     public class ProductType
     {
         public int Id { get; set; }
         public string Name { get; set; }
-<<<<<<< HEAD
-        //-------------------------------------------------------------------------------
 
-=======
->>>>>>> master
     }
 }
